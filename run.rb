a8d5aabb7eacd8c438f4011ff059b020e863fe54
Sedrick <digitalphenom0@gmail.com>
2025-08-26 23:01:00 -0700

    set configure files and initial root and home routes pointing to home.erb

--- run.rb
+++ run.rb
@@ -1,2 +1,26 @@
 
-requir
+require 'erubi'
+require 'bundler/setup'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'rack-livereload'
+
+configure do
+  use Rack::LiveReload
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+  set :public_folder, File
+    .expand_path('../harbor_stucco/public', __dir__)
+  set :views, File.expand_path('../harbor_stucco/views', __dir__)
+  #set :erb, escape_html: true
+end
+
+# ‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
+
+get '/' do
+  redirect '/home'
+end
+
+get '/home' do
+  erb :"home.html", layout: :"layout.html"
+end
