057fbab93eaa7b21c25a91b9ae18ba727ff15b7c
Sedrick <digitalphenom0@gmail.com>
2025-09-04 16:25:14 -0700

    defined helper that streamlines each stucco textures display - initialized `row` to an array ds that segments textures by nested arrays each representing a row - defined `#display_finishes`, which contains the logic for streamlining display - defined `#format_image`. accept a texture name and formats it to retreive the proper image name

--- app/helpers/view_helpers.rb
+++ app/helpers/view_helpers.rb
@@ -20,4 +20,41 @@ module ViewHelpers
     path = File.join(File.expand_path('../../views/', __dir__), file)
     File.read(path)
   end
+
+  def format_image(name)
+    name.split.join('-')
+  end
+
+  def display_finishes
+    rows = [
+      ['smooth finish', 'santa barbara', 'arizona finish'],
+      ['cat face', 'monterey finish', 'trowel sweep'],
+      ['spanish finish', 'fine sand', 'english finish']
+    ]
+
+    result = rows.map.with_index do |row, idx|
+      row.map do |name|
+      formatted_name = format_image(name)
+        "<div class=\"div-block-9 mobiletexturehide\">
+          <h4 class=\"heading-24\">#{name.upcase}</h4>
+          <img
+            class=\"image-4\"
+            loading=\"lazy\"
+            src=\"images/#{formatted_name}.png\"
+            srcset=\"
+              images/#{formatted_name}.png 676w\
+              images/#{formatted_name}-p-500.png 500w
+              images/#{formatted_name}-p-130x130q80.png 130w\"
+            sizes=\"
+              (max-width: 479px) 100vw,
+              (max-width: 767px) 33vw,
+              (max-width: 991px) 160px, 234px\"
+            alt=\"#{formatted_name}\">
+        </div>"
+      end
+    end
+    result.each.with_index(1) do |row, idx|
+      yield(row, idx)
+    end
+  end
 end
